404c21730e4f74b271c0ac57daa2ce3a6b3fa9f6
Ojasveer955 <68781367+Ojasveer955@users.noreply.github.com>
2025-03-09 00:22:05 +0530

    Images are getting pulled from Firebase database. Working on physical device.
    Need to add more jobs to the database.

--- app/src/main/java/com/example/sidehustle/adapter/JobAdapter.java
+++ app/src/main/java/com/example/sidehustle/adapter/JobAdapter.java
@@ -1,15 +1,22 @@
 package com.example.sidehustle.adapter;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.example.sidehustle.R;
 import com.example.sidehustle.model.Job;
 
@@ -39,12 +46,51 @@ public class JobAdapter extends RecyclerView.Adapter<JobAdapter.JobViewHolder> {
         holder.jobLocation.setText(job.getLocation());
         holder.jobSalary.setText(job.getSalary());
 
-        // Load image from URL using Glide
-        Glide.with(context)
-                .load(job.getImageUrl())  // Load image from Firestore URL
-                .placeholder(R.drawable.placeholder_image) // Add a placeholder image
-                .error(R.drawable.error_image) // Add an error image if the URL is broken
+        String driveUrl = job.getImageUrl();
+        if (driveUrl != null && !driveUrl.isEmpty()) {
+            String directImageUrl = convertDriveUrlToDirectUrl(driveUrl);
+            Log.d("ImageLoading", "Original URL: " + driveUrl);
+            Log.d("ImageLoading", "Converted URL: " + directImageUrl);
+            
+            Glide.with(context)
+                .load(directImageUrl)
+                .placeholder(R.drawable.placeholder_image)
+                .error(R.drawable.error_image)
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model,
+                            Target<Drawable> target, boolean isFirstResource) {
+                        Log.e("ImageLoading", "Failed to load image: " + model + 
+                              "\nException: " + (e != null ? e.getMessage() : "null"), e);
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model,
+                            Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        Log.d("ImageLoading", "Image loaded successfully");
+                        return false;
+                    }
+                })
+                .timeout(60000) // Increase timeout to 60 seconds
+                .centerCrop()
                 .into(holder.companyLogo);
+        } else {
+            Log.w("ImageLoading", "No image URL provided");
+            holder.companyLogo.setImageResource(R.drawable.placeholder_image);
+        }
+    }
+
+    private String convertDriveUrlToDirectUrl(String driveUrl) {
+        String fileId = "";
+        if (driveUrl.contains("drive.google.com/file/d/")) {
+            fileId = driveUrl.split("/file/d/")[1].split("/")[0];
+        } else if (driveUrl.contains("drive.google.com/open?id=")) {
+            fileId = driveUrl.split("open\\?id=")[1];
+        }
+        
+        // Using an alternative format that might work better on real devices
+        return "https://lh3.googleusercontent.com/d/" + fileId;
     }
 
     @Override
